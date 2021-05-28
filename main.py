2886f26f4672c26228167520e62d524adabc6a03
Sean <sean.remenyi@gmail.com>
2021-05-28 23:33:07 +0000

    cards and deal

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import art
+import random
 
 print(art.logo)
+
+cards = ["A","1","2","3","4","5","6","7","8","9","J","Q","K"]
+suits = ["♥","♧","♢","♤"]
+deck = []
+for suit in suits:
+    for card in cards:
+        deck.append(card+suit)
+
+def deal(amount, hand):
+    for cards in range(0,amount):
+        hand.append(random.choice(deck))
+
+user_hand = []
+deal(2,user_hand)
+comp_hand = []
+deal(2,comp_hand)
+
